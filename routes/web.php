952162e09c9636967fb72a2d94703716c1cb9d45
assanihulda <huldaassani@gmail.com>
2025-05-02 18:23:56 +0100

    Initial commit of ticketing project

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,13 @@ use App\Http\Controllers\ParticipantController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+//users
 Route::get('/', [EventController::class, 'publicIndex'])->name('events.public');
 Route::get('/events/{event}', [EventController::class, 'show'])->name('events.show');
+Route::post('/events/{event}/participate', [ParticipantController::class, 'store'])->name('events.participate');
 
+// administrateur
 Route::get('/admin', [EventController::class, 'index'])->name('events.index');
-
 Route::get('/admin/events/create', [EventController::class, 'create'])->name('events.create');
 Route::post('/admin/events', [EventController::class, 'store'])->name('events.store');
 Route::get('/admin/events/{event}/edit', [EventController::class, 'edit'])->name('events.edit');
@@ -26,11 +28,6 @@ Route::delete('/admin/events/{event}', [EventController::class, 'destroy'])->nam
 
 Route::get('/admin/participants', [ParticipantController::class, 'index'])->name('participants.index');
 
-// Soumission d’un participant à un événement public
-Route::post('/events/{event}/participate', [ParticipantController::class, 'store'])->name('events.participate');
 
-// Statistiques admin (ex : nombre de participants par événement)
-Route::get('/admin/statistics', [EventController::class, 'statistics'])->name('admin.statistics');
 
-// Formulaire de participation à un événement
 
